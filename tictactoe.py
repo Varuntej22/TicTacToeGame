c6da0987ee62d5400370065f2724db6c707f31be
Varuntej22 <159868897+Varuntej22@users.noreply.github.com>
2024-08-21 23:44:02 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,8 @@
-# --------- Global Variables -----------
 
-# Will hold our game board data
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
 
-# Lets us know if the game is over yet
 game_still_going = True
 
 # Tells us who the winner is
@@ -14,35 +11,23 @@ winner = None
 # Tells us who the current player is (X goes first)
 current_player = "X"
 
-
-# ------------- Functions ---------------
-
-# Play a game of tic tac toe
 def play_game():
 
-  # Show the initial game board
   display_board()
 
-  # Loop until the game stops (winner or tie)
   while game_still_going:
-
-    # Handle a turn
+           
     handle_turn(current_player)
 
-    # Check if the game is over
     check_if_game_over()
 
-    # Flip to the other player
     flip_player()
   
-  # Since the game is over, print the winner or tie
   if winner == "X" or winner == "O":
     print(winner + " won.")
   elif winner == None:
     print("Tie.")
 
-
-# Display the game board to the screen
 def display_board():
   print("\n")
   print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
@@ -51,14 +36,11 @@ def display_board():
   print("\n")
 
 
-# Handle a turn for an arbitrary player
 def handle_turn(player):
 
-  # Get position from player
   print(player + "'s turn.")
   position = input("Choose a position from 1-9: ")
 
-  # Whatever the user inputs, make sure it is a valid input, and the spot is open
   valid = False
   while not valid:
 
@@ -66,16 +48,13 @@ def handle_turn(player):
     while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
       position = input("Choose a position from 1-9: ")
  
-    # Get correct index in our board list
     position = int(position) - 1
 
-    # Then also make sure the spot is available on the board
     if board[position] == "-":
       valid = True
     else:
       print("You can't go there. Go again.")
 
-  # Put the game piece on the board
   board[position] = player
 
   # Show the game board
@@ -90,9 +69,8 @@ def check_if_game_over():
 
 # Check to see if somebody has won
 def check_for_winner():
-  # Set global variables
   global winner
-  # Check if there was a winner anywhere
+  # Check if there was a winner a
   row_winner = check_rows()
   column_winner = check_columns()
   diagonal_winner = check_diagonals()
